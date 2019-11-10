7432e0d114306f435a80ea6705e24f9af6583b00
Aneli <aneli.klev.a@gmail.com>
2019-11-11 02:39:45 +0300

    chat echo server

--- client.py
+++ client.py
@@ -7,17 +7,24 @@ PORT = 9095
 
 async def tcp_echo_client(host, port):
     reader, writer = await asyncio.open_connection(host, port)
-    message = 'Hello, world'
+    
 
-    writer.write(message.encode())
-    await writer.drain()
+    while True:
+            data = await reader.read(1024)
+            print(f'SERVER: {data.decode()}')
+            message = input()
+            writer.write(message.encode())
+            await writer.drain()
+            if message == 'exit' or data =='':
+                data = await reader.read(18)
+                print(f'SERVER: До свидания! Надеюсь, еще увидимся!')
+                writer.close()
+                break
+    
 
-    data = await reader.read(100)
-    writer.close()
     # await writer.wait_closed()
 
-# asyncio.run(tcp_echo_client(HOST, PORT))
+if __name__ == "__main__":
+   
+    asyncio.run(tcp_echo_client(HOST, PORT))
 
-loop = asyncio.get_event_loop()
-task = loop.create_task(tcp_echo_client(HOST, PORT))
-loop.run_until_complete(task)
--- server.py
+++ server.py
@@ -1,31 +1,144 @@
 import asyncio
+import pickle
+import os
+from hashlib import md5
 
 HOST = 'localhost'
 PORT = 9095
+H_FILE = "hosts.pickle"
+CHAT = "global.txt" 
+
+async def auth(writer, reader, name):
+    """
+    Функция аутентификации
+    """
+    global clients
+
+    print(name)
+    if name in clients.keys():
+        # проверка нахождения пользователя в файле системы
+        user_name =  clients[name][0].decode()
+
+        writer.write(f"Рад вновь вас здесь приветствовать, {user_name}.\nВведите, пожалуйста, пароль, чтобы я точно вас узнал...  ".encode())
+        await writer.drain()
+        
+        while True:
+            pas = await reader.read(100)
+            h_pas = md5(pas).hexdigest()
+            if h_pas == clients[name][1]:
+                writer.write("""Ваш пароль верен! \nТеперь я полностью уверен, что зрение меня не подвело, и это вы!""".encode())
+                await writer.drain()   
+                break
+
+            else:
+                writer.write("""Ваш пароль не верен! 
+                Повторите попытку и не дайте мне усомниться,что это вы!""".encode())
+                await writer.drain()
+    else:
+        # авторизация нового пользователя 
+        writer.write("Здравствуйте, мы с вами не знакомы, но сейчас это исправим ".encode())
+        await writer.drain()
+        writer.write("\nВведите свое имя: ".encode())
+        await writer.drain()
+        log = await reader.read(100)
+        print(log)
+        writer.write("\nВведите пароль, чтобы я в будущем вас точно узнал. ".encode())
+        await writer.drain()
+        pas = await reader.read(100)
+        clients[name] = (log, md5(pas).hexdigest())
+        print(clients)
+
+        # вносим нового пользователя в файл для отслеживания
+        with open(H_FILE,"wb") as file:
+            pickle.dump(clients,file)
+        
+        writer.write(f"Поздравляю, {log.decode()}, теперь я однозначно тебя узнаю! ".encode())
+        await writer.drain()
+    
+
+async def down(name, mes:str):
+    """
+    Запись сообщения в файл
+    """
+    with open (CHAT,"a") as f:
+        print(f"{name}>> {mes.decode()}",file=f)
+
+# async def up():
+#     """
+#     Возврат всего чата
+
+#     """
+async def chat(writer, reader, user, par):
+    """
+    Функция для общения и доступа к общему чату
+    """
+    if par == '1':
+        writer.write('Введите сообщение, любезнейший: '.encode())
+        await writer.drain()
+        mes = await reader.read(100)
+        print(mes.decode())
+        await down(user,mes)
+        print("received %s from %s" % (mes.decode(), user))
+        
+
+    elif par == '2':
+        with open(CHAT,'r') as f:
+            for i in f:
+                writer.write(i.encode())
+                
+    else:
+        writer.write("Я вас не понял, может, повторите?..".encode())
+        await writer.drain()
+        
+
 
 
 async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
+    global clients 
 
-    writer.write(data)
+    addr = writer.get_extra_info("peername")
+    
+    await auth(writer,reader,addr[0])
+
+    writer.write("""\nМеню:
+        Введите '1', чтобы написать в общий чат. 
+        Если вы захотите просмотреть историю сообщений чата, введите 2. 
+        Для выхода введите 'exit'. """.encode())
     await writer.drain()
+   
+    par = await reader.read(4)
+    par_d  = par.decode()
+    while par_d != "exit":
+        par_d  = par.decode()
+        await chat(writer, reader, clients[addr[0]][0].decode(), par_d)
+        writer.write("""\nМеню:
+        Введите '1', чтобы написать в общий чат. 
+        Если вы захотите просмотреть историю сообщений чата, введите 2. 
+        Для выхода введите 'exit'. """.encode())
+        par = await reader.read(200)
 
     writer.close()
+    
 
 
-loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, HOST, PORT, loop=loop)
-server = loop.run_until_complete(coro)
 
-# Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+async def main():
+    
+    server = await asyncio.start_server(handle_echo, HOST, PORT)
+    
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    async with server:  
+        await server.serve_forever()
 
-# Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+
+if __name__ == "__main__":
+
+    with open (CHAT,"w") as f:
+        print("",file=f)
+    # проверка на наличие информации в файлеы
+    if os.path.getsize(H_FILE) > 0: 
+        with open(H_FILE,"rb") as file:
+            clients = pickle.load(file)
+    else:
+        clients = {}     
+    asyncio.run(main())
